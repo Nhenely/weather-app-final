587ac8693a28cb4f3edf6fda1fd50d7f703e43b9
Nhenely <nicole.henely@gmail.com>
2021-12-18 10:41:30 -0600

    Added min and max temperature and celcius conversion

--- src/app.js
+++ src/app.js
@@ -24,14 +24,20 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let conditionElement = document.querySelector("#condition");
+  let minElement = document.querySelector("#min");
+  let maxElement = document.querySelector("#max");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
 
   fahrenheitTemperature = response.data.main.temp;
+  fahrenheitMin = response.data.main.temp_min;
+  fahrenheitMax = response.data.main.temp_max;
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
   cityElement.innerHTML = response.data.name;
+  minElement.innerHTML = Math.round(fahrenheitMin);
+  maxElement.innerHTML = Math.round(fahrenheitMax);
   conditionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
@@ -55,20 +61,32 @@ function handleSubmit(event) {
 function displayCelciusTemp(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
+  let minElement = document.querySelector("#min");
+  let maxElement = document.querySelector("#max");
   fahrenheitLink.classList.remove("active");
   celciusLink.classList.add("active");
   let celciusTemperature = ((fahrenheitTemperature - 32) * 5) / 9;
+  let celciusMin = ((fahrenheitMin - 32) * 5) / 9;
+  let celciusMax = ((fahrenheitMax - 32) * 5) / 9;
   temperatureElement.innerHTML = Math.round(celciusTemperature);
+  minElement.innerHTML = Math.round(celciusMin);
+  maxElement.innerHTML = Math.round(celciusMax);
 }
 function displayFahrenheitTemp(event) {
   event.preventDefault();
   fahrenheitLink.classList.add("active");
   celciusLink.classList.remove("active");
   let temperatureElement = document.querySelector("#temperature");
+  let minElement = document.querySelector("#min");
+  let maxElement = document.querySelector("#max");
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+  minElement.innerHTML = Math.round(fahrenheitMin);
+  maxElement.innerHTML = Math.round(fahrenheitMax);
 }
 
 let fahrenheitTemperature = null;
+let fahrenheitMin = null;
+let fahrenheitMax = null;
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
