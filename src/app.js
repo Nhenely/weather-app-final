19f65619f8a35c313e2cfe0382877d620f317d97
Nhenely <nicole.henely@gmail.com>
2021-12-20 13:24:41 -0600

    Integrated API into daily forecast

--- src/app.js
+++ src/app.js
@@ -20,32 +20,48 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
     <div class="col-2">
-      <div class="weather-forecast-date">${day}</div>
+      <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
       <img
-        src="https://ssl.gstatic.com/onebox/weather/48/partly_cloudy.png"
+        src="https://openweathermap.org/img/wn/${
+          forecastDay.weather[0].icon
+        }@2x.png"
         alt=""
         width="36"
       />
       <div class="weather-forecast-temp">
-        <span class="weather-forecast-temp-max">20º</span>
-        <span class="weather-forecast-temp-min">15º</span>
+        <span class="weather-forecast-temp-max">${Math.round(
+          forecastDay.temp.max
+        )}º</span>
+        <span class="weather-forecast-temp-min">${Math.round(
+          forecastDay.temp.min
+        )}º</span>
       </div>
     </div>`;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
+
 function getForecast(coordinates) {
   let apiKey = "ff769fda7e31b5b213b488e026c99310";
   let apiUrl = `
